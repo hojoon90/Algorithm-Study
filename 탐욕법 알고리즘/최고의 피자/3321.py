f38476d5ace8196610fd4aca2c73133e683bb888
최호준 <hojoon90@gmail.com>
2022-05-22 23:05:37 +0900

    탐욕법-최고의 피자 풀이 및 회고 작성 코드 일부 수정

--- 탐욕법 알고리즘/최고의 피자/3321.py
+++ 탐욕법 알고리즘/최고의 피자/3321.py
@@ -10,15 +10,14 @@ for i in range(topingCnt):
 # 토핑을 하나씩 더하면서 평균이 가장 높은 값을 출력해줌.
 array.sort(reverse=True)
 
-avg = 0.0
+avg = douCalories / douPrice
 calories = douCalories
-for j in range(topingCnt + 1):
-    if j == topingCnt:
-        break
+coast = 0
 
-    if j != 0:
-        calories += array[j-1]
-    totalPrice = douPrice + topingPrice * j
+for j in array:
+    calories += j
+    coast += topingPrice
+    totalPrice = douPrice + coast
 
     compAvg = calories / totalPrice
     if compAvg > avg:
