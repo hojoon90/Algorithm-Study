91a8a9e5269ad8e2bd0e0d01a38dfdd48c668b07
최호준 <hojoon90@gmail.com>
2022-07-09 23:44:07 +0900

    프로그래머스 조이스틱 풀이중.....화난다

--- 탐욕법 알고리즘/조이스틱/prog42860.py
+++ 탐욕법 알고리즘/조이스틱/prog42860.py
@@ -3,22 +3,27 @@ nameArr = list(name)
 
 defaultVal = 65;
 count = 0
-i = 0
+next = 0
 
-for name in nameArr:
+min_move = len(nameArr) - 1
+
+while name[min_move] == 'A' and min_move > 0:
+    min_move -= 1
+
+for i, name in enumerate(nameArr):
     nameOrd = ord(name)
     nameDif = nameOrd - defaultVal
-    if nameOrd == 65:
-        i = i+1
-        continue
 
     if nameDif > 13:
         count += abs(91 - nameOrd)
     else:
         count += nameDif
 
-    if len(nameArr)-1 != i:
-        count += 1
-    i = i+1
+    next = i + 1
+    while next < len(nameArr) and nameArr[next] == 'A':
+        next += 1
 
+    min_move = min(min_move, i + (i + len(nameArr)) - next)
+
+count += min_move
 print(count)
