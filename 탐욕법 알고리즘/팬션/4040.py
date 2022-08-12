5b75e0abf362232776ab3cf6679bc170c07ddb1c
최호준 <hojoon90@gmail.com>
2022-08-12 23:50:46 +0900

    문제 분석겸 풀이중

--- 탐욕법 알고리즘/팬션/4040.py
+++ 탐욕법 알고리즘/팬션/4040.py
@@ -1,2 +1,12 @@
 n, m = map(int,input().split())
-i, j = map(int,input().split())
+roomList = [];
+
+
+for i in range(n):
+    room = input();
+    roomList.append(room)
+
+s, t = map(int,input().split())
+
+
+
