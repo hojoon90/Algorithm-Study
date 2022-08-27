38d714a8fc7546d9e62f6b8386bb7e43233f0ec7
최호준 <hojoon90@gmail.com>
2022-08-27 23:22:15 +0900

    문제 풀이 다시 진행

--- 탐욕법 알고리즘/자물쇠/4684.py
+++ 탐욕법 알고리즘/자물쇠/4684.py
@@ -5,4 +5,21 @@ originList = []
 for i in range(n):
     originList.append(i)
 
-#dfs 접근...?
+# dfs 접근
+
+changeList = m
+
+lastLeftCount = 0;
+# 오른쪽으로 다시 돌리는 for문 (상위 카운트)
+# 접근 방법
+# 1. 맨 마지막 왼쪽으로 밀기를 다시 오른쪽으로 민다 (카운트가 제일 늦게 올라감)
+# 2. 위치를 서로 바꾸는 작업 실행 (두번째로 늦게 올라가는 카운트)
+# 3. 맨처음 왼쪽으로 밀기를 다시 오른쪽으로 민다 (제일 빨리 올라가는 카운트)
+
+while True:
+    lastLeftCount += 1
+    for j in range(n):
+        if(j == 0):
+            m.insert(n, m[0])
+            m.pop(0)
+
